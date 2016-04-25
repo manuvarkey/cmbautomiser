7dacaeeca9fd39ac4a3972f37747622a48cb481a
manuvarkey <manuvarkey@gmail.com>
2016-04-25 11:44:32 +0530

    * Bug fixes restore entry support for rate,qty

--- cmbautomiser/bill_dialog.py
+++ cmbautomiser/bill_dialog.py
@@ -67,13 +67,13 @@ class BillDialog:
         cellrenderers = []
         # call backs
         def callback_agmntno(value, row):
-            return to_unicode(populated_items[row][1])  # hack for unicode support
+            return populated_items[row][1]  # hack for unicode support
 
         def callback_description(value, row):
-            return to_unicode(populated_items[row][2])  # hack for unicode support
+            return populated_items[row][2]  # hack for unicode support
 
         def callback_unit(value, row):
-            return to_unicode(populated_items[row][3])  # hack for unicode support
+            return populated_items[row][3]  # hack for unicode support
 
         def callback_rate(value, row):
             if populated_items[row][4] != 0:
